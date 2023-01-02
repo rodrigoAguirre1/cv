92ed77446756e3886eea6b21037b13f79eb57deb
rodri <rodri@DESKTOP-OU2H08P.localdomain>
2023-01-02 15:28:39 -0300

    added fixes

--- terraform/main.tf
+++ terraform/main.tf
@@ -113,6 +113,12 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
     max_ttl                = 86400
   }
 
+  custom_error_response {
+    error_code = 403
+    response_code = 200
+    response_page_path = "/index.html"
+  }
+
   tags = {
     Name = "The Cloudfront distribution for my static web bucket"
   }
